5babee22a7b06924598bd105c4825c0d8f90af82
“yingzhang0897” <yinganticpc@gmail.com>
2025-06-04 10:22:55 +0800

    remove swaggerDocument

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,6 +2,5 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
