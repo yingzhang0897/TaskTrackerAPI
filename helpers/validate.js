5bf9277ac0da1cf7ff63b840b9471d3f50564b1d
“yingzhang0897” <yinganticpc@gmail.com>
2025-06-06 11:00:29 +0800

    add custom rule to check mongoId

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,6 +1,11 @@
 const Validator = require('validatorjs');
 const mongoose = require('mongoose');
 
+// Custom rule to check MongoDB ObjectId
+Validator.register('mongoId', function (value, requirement, attribute) {
+  return mongoose.Types.ObjectId.isValid(value);
+}, 'The :attribute must be a valid MongoDB ObjectId.');
+
 // Custom rule for array of ObjectIds
 Validator.register('objectIdArray', function (value, requirement, attribute) {
   return Array.isArray(value) && value.every(id => mongoose.Types.ObjectId.isValid(id));
