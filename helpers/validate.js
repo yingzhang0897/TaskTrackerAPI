63f13591c8d0e7dae599d50e6b07c127dcf2b09b
“yingzhang0897” <yinganticpc@gmail.com>
2025-06-06 10:56:09 +0800

    add mongoose

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,4 +1,11 @@
 const Validator = require('validatorjs');
+const mongoose = require('mongoose');
+
+// Custom rule for array of ObjectIds
+Validator.register('objectIdArray', function (value, requirement, attribute) {
+  return Array.isArray(value) && value.every(id => mongoose.Types.ObjectId.isValid(id));
+}, 'The :attribute must be an array of valid ObjectIds.');
+
 const validator = (body, rules, customMessages, callback) => {
   const validation = new Validator(body, rules, customMessages);
   validation.passes(() => callback(null, true));
